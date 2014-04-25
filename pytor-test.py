2e1f4e70bc5b0ec313d3f8e5f39252a0f2a0997e
bdheath <brad.heath@gmail.com>
2014-04-25 10:34:28 -0400

    Updated test

--- pytor-test.py
+++ pytor-test.py
@@ -1,6 +1,12 @@
 from pytor import pytor
+import urllib2
 
+ip = urllib2.urlopen('http://bradheath.org/ip').read()
 tor = pytor()
 
-print "My IP address is: " + tor.ip()
+
+print "Without Tor, my IP address is     : " + ip
+print "With Tor, my IP address is        : " + tor.ip()
+tor.newIdentity()
+print "Now my IP address is              : " + tor.ip()
 
