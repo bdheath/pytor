61dc791638984f0f87423c553328626b762337a6
Brad Heath <brad.heath@gmail.com>
2017-08-22 15:45:53 -0400

    improvements

--- pytor-test.py
+++ pytor-test.py
@@ -2,8 +2,7 @@ from pytor import pytor
 import urllib2
 
 ip = urllib2.urlopen('http://bradheath.org/ip').read()
-tor = pytor()
-
+tor = pytor(controller=True, controlPort = 9051, port = 9060, password='YOURPASSWORD')
 
 print "Without Tor, my IP address is     : " + ip
 print "With Tor, my IP address is        : " + tor.ip()
--- pytor.py
+++ pytor.py
@@ -10,20 +10,19 @@
 #
 #
 
-import socks
-import socket
 import stem
-import urllib2
 from stem.control import Controller
 from stem import Signal
 import mechanize
 import datetime
+import requests
 
 class pytor:
 
 	controlPort = 9051
 	port = 9050
 	host = 'localhost'
+	password = ''
 	_version = 0.1
 	_last_result = None
 	_last_request = None
@@ -32,47 +31,72 @@ class pytor:
 	_ip = None
 	_connected = False
 	browser = None
+	controller = False
 	
-	try:
-		torControl = Controller.from_port(port=controlPort)
-		_connected = True
-	except:
-		_connected = False
+	_proxies = {}
 
-	def __init__(self, host='localhost', port=9050):
-		if self._connected:
-			socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, host, port)
-			socket.socket = socks.socksocket
-			self._last_id_time = datetime.datetime.now()
-		else:
-			print "### ERROR: Could not establish a Tor connection!"
+	def __init__(self, host='localhost', port=9050, controller=False, controlPort = 9051, password=''):
+		self.controlPort = controlPort
+		self.port = port
+		self.controller = controller
+		self._proxies['http'] = 'socks5://localhost:' + str(self.port)
+		self._proxies['https'] = 'socks5://localhost:' + str(self.port)
+		if self.controller:
+			self.torControl = Controller.from_port(port=controlPort)
+			self.torControl.authenticate(self.password)
+			self._connected = True
+		self._last_id_time = datetime.datetime.now()
 		return
-		
-	def get(self, url):		
+	
+	def request(self, url, timeout = 30):
 		self._checkIdentityTime()
-		request = urllib2.Request(url)
-		request.add_header('Cache-Control','max-age=0')
-		response = urllib2.urlopen(request)
-		self._last_result = response.read()
+		r = requests.get(url, timeout=timeout, proxies=self._proxies)
 		self._last_request = url
-		return self._last_result
+		self._last_result = r.text
+		return r
+	
+	def get(self, url, t=30):		
+		self._checkIdentityTime()
+
+		r = self.request(url, timeout = t)
+		
+		if r.status_code == 200:
+			self._last_result = r.text
+			self._last_request = url
+			return self._last_result
+		else:
+			return False
 
 	def ip(self):
 		self._ip = self.get('http://bradheath.org/ip')
 		return self._ip
 
+	def timeForNew(self):
+		self._checkIdentityTime()
+		return		
+		
+	def saveLastResult(file):
+		with open(file, 'wb') as local_file:
+			local_file.write(self._last_result)
+		return
+		
 	def downloadFile(self, url, file):
 		self._checkIdentityTime()
-		request = urllib2.Request(url)
-		request.add_header('Cache-Control','max-age=0')
-		request.add_header('User-agent', 'Mozilla/5.0')
-		response = urllib2.urlopen(request)
+		r = requests.get(url, stream = True, proxies = self._proxies)
 		with open(file, 'wb') as local_file:
-			local_file.write(response.read())
+			for chunk in r.iter_content ( chunk_size = 1024)
+				if chunk:
+					local_file.write(chunk)
+		self._last_request = url
 		return True
 
 	def newIdentity(self):
-		self.torControl.authenticate()
+		print "## NEW IDENTITY ## "
+		if not self._connected:
+			self.torControl = Controller.from_port(port=self.controlPort)
+			self.torControl.authenticate(self.password)
+			self._connected = True
+		self.torControl.authenticate(self.password)
 		self.torControl.signal(Signal.NEWNYM)
 		self._last_id_time = datetime.datetime.now()
 		return True
